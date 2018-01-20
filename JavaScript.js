7ac582e46e9ab3f93f424f0c481e3a8a025527ac
Brian Strylowski <brianstry@hotmail.com>
2018-01-20 18:43:19 -0500

    seconed

--- JavaScript.js
+++ JavaScript.js
@@ -15,34 +15,37 @@ var correctAnswersArray = [];
 var counter = 0;
 var correctAnswersCounter = 0;
 var wrongAnswersCounter = 0;
-
-
-
-
-$("#question")
-
+var timeOutAnswers = 0;
 
 
 
 
 
 
+function moreQuestions(){
+    if(counter === correctAnswersArray.length){
+        alert("trivia complete!");
+    } else {
+        eachRound();
+    }
+}
 
+function eachRound(){
 
+    $("#question").text(questionArray[counter]);
+    $("#buttonOne").text(AnswerArray[counter][0]);
+    $("#buttonTwo").text(AnswerArray[counter][1]);
+    $("#buttonThree").text(AnswerArray[counter][2]);
 
+    setTimeout(timeOut, 1000 * 60);
 
+}
 
-
-
-
-
-
-
-
-
-
-
-
+function timeOut(){
+    timeOutAnswers++;
+    counter++;
+    moreQuestions();
+}
 
 
 
@@ -50,23 +53,41 @@ $("#question")
 $("#buttonOne").on("click", function () {
     if (answerArray[counter][0] === correctAnswers[counter]){
         alert("correct");
+        correctAnswersCounter++;
+        counter++;
+        moreQuestions();
     } else {
         alert("incorrect")
+        wrongAnswersCounter++;
+        counter++;
+        moreQuestions();
     }
 })
 
 $("#buttonTwo").on("click", function () {
     if (answerArray[counter][1] === correctAnswers[counter]){
         alert("correct");
+        correctAnswersCounter++;
+        counter++;
+        moreQuestions();
     } else {
         alert("incorrect")
+        wrongAnswersCounter++;
+        counter++;
+        moreQuestions();
     }
 })
 
 $("#buttonThree").on("click", function () {
     if (answerArray[counter][2] === correctAnswers[counter]){
         alert("correct");
+        correctAnswersCounter++;
+        counter++;
+        moreQuestions();
     } else {
         alert("incorrect")
+        wrongAnswersCounter++;
+        counter++;
+        moreQuestions();
     }
 })
